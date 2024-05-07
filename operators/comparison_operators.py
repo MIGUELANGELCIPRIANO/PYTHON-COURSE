c78909f5ae007b9e80f996702b36da234dc062eb
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2024-05-07 18:44:01 -0300

    FEAT: comparison operators

---
+++ operators/comparison_operators.py
@@ -0,0 +1,26 @@
+# All return Booleans
+
+# EQUAL
+equal = 5 == 6
+
+# DIFFERENT
+different = 5 != 6
+
+# GREATER
+greater = 5 > 6
+
+# LESS
+less = 5 < 6
+
+# GREATER OR EQUAL
+greater_or_equal = 5 >= 6
+
+# LESS OR EQUAL
+less_or_equal = 5 <= 6
+
+print(equal)
+print(different)
+print(greater)
+print(less)
+print(greater_or_equal)
+print(less_or_equal)
