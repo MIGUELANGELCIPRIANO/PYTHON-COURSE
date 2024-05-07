feaaf944a243ef927499432cd8a3898fca2f5d8d
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2024-05-07 18:31:03 -0300

    FEAT: arithmetic operators

---
+++ operators/arithmetic_operators.py
@@ -0,0 +1,18 @@
+# ADDITION & SUBTRACTION
+addition = 12 + 6
+subtraction = 12 - 6
+
+# MULTIPLICATION & DIVISION
+multiplication = 12 * 6
+division = 12 / 6 # Returns a float data
+
+# EXPONENTIATION
+exponentiation = 12 ** 6
+
+# LOW DIVISION
+low_division = 12 // 6 # Returns a division's low integer
+
+# MODULE
+module = 12 % 6 # Returns a division's remainder
+
+print(division)
