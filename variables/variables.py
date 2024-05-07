9f1e452d2b6774e8fcfe6c5fdfad9e6eb11b9b44
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2024-05-07 17:12:41 -0300

    FEAT: variables

---
+++ variables/variables.py
@@ -0,0 +1,24 @@
+# camelCase variable
+userFullName = 'John Doe'
+
+# snake_case variable (recommended)
+user_full_name = 'John Doe'
+
+number = 10
+number += 5
+number += 5
+# del number
+print(number)
+
+name = 'John'
+
+# Concatenate with f.strings
+greeting = f'Hi {name}, How are you?'
+
+# Concatenate with +
+greeting = 'Hi ' + name + ', How are you?'
+
+print(greeting)
+
+# Ownership operators (in / not in)
+print("Peter" not in greeting)
