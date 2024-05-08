bbaeb1f5d95fe495a205472124372d06d9609610
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2024-05-08 11:28:26 -0300

    FEAT: logical operators

---
+++ operators/logical_operators.py
@@ -0,0 +1,20 @@
+# AND
+
+result1 = True & True  # Return True
+result2 = False & True  # Return False
+result3 = True & False  # Return False
+result4 = False & False  # Return False
+
+# OR
+
+result5 = True | True  # Return True
+result6 = False | True  # Return True
+result7 = True | False  # Return True
+result8 = False | False  # Return False
+
+# NOT
+
+result9 = not True  # Return False
+result10 = not False  # Return True
+
+print(result1)
