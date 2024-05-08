45c63bc27e6dd03fb3e6a0edf66a2f171b8dd5fd
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2024-05-08 15:47:22 -0300

    FEAT: data entry (inputs)

---
+++ inputs/input.py
@@ -0,0 +1,6 @@
+username = input("Enter your username: ")  # Always returns string data type
+print(f"Username: {username}")
+
+number = input("Enter number= ")
+print(number * 2)  # In this case, "number" is a string data type
+print(int(number) * 2)  # In this case, "number" is an integer data type
