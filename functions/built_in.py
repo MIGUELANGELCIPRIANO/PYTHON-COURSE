0616755121ae9fe5d07afb5dd0fd4fba0ba56c18
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2024-05-11 18:10:28 -0300

    FEAT: Built-in functions

---
+++ functions/built_in.py
@@ -0,0 +1,23 @@
+numbers = [3.5, 5.5, 4.5, 4.4, 6.6]
+
+# MAX()
+print(max(numbers))  # Largest number of an iterable
+
+# MIN()
+print(min(numbers))  # Smallest number of an iterable
+
+# ROUND()
+print(
+    round(12.345678, 2)
+)  # Round the number taking into account the second parameter specified as the number of decimal places
+
+# BOOL
+print(bool(None))  # Returns False if the specified parameter is 0, null, False or none
+
+# ALL
+print(
+    all(["Elesh-Norn", "Jin-Gitaxias", "Sheoldred", "Urabrask", "Vorinclex"])
+)  # Returns True if all values of the iterable are true
+
+# SUM
+print(sum(numbers))  # Sum all the numbers of an iterable
