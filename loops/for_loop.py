28c39d45586b935dfb2494f9072341ee3ce44ea8
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2024-05-10 21:04:27 -0300

    FEAT: For loop in one line code

--- loops/for_loop.py
+++ loops/for_loop.py
@@ -3,61 +3,61 @@
 praetors = ["Elesh-Norn", "Jin-Gitaxias", "Sheoldred", "Urabrask", "Vorinclex"]
 numbers = [3.5, 5.5, 4.5, 4.4, 6.6]
 
-# for praetor in praetors:
-#     print(praetor)
+for praetor in praetors:
+    print(praetor)
 
-# for number in numbers:
-#     product = number * 10
-#     print(product)
+for number in numbers:
+    product = number * 10
+    print(product)
 
-# # ZIP()
+# ZIP()
 
-# for praetor, number in zip(praetors, numbers):
-#     print(f"Phyrexian Praetor: {praetor}")
-#     print(f"Power/Toughness: {number}")
+for praetor, number in zip(praetors, numbers):
+    print(f"Phyrexian Praetor: {praetor}")
+    print(f"Power/Toughness: {number}")
 
-# # RANGE()
+# RANGE()
 
-# for number in range(10, 20):
-#     print(number)
+for number in range(10, 20):
+    print(number)
 
-# for number in range(len(numbers)):  # Non-optimal way to iterate lists!
-#     print(numbers[number])
+for number in range(len(numbers)):  # Non-optimal way to iterate lists!
+    print(numbers[number])
 
-# # ENUMERATE()
+# ENUMERATE()
 
-# for number in enumerate(numbers):
-#     index = number[0]
-#     value = number[1]
-#     print(f"index = {index}, value = {value}")
+for number in enumerate(numbers):
+    index = number[0]
+    value = number[1]
+    print(f"index = {index}, value = {value}")
 
-# # FOR ELSE
+# FOR ELSE
 
-# for praetor, number in zip(praetors, numbers):
-#     print(f"Phyrexian Praetor: {praetor}")
-#     print(f"Power/Toughness: {number}")
-# else:
-#     print("End of list")
+for praetor, number in zip(praetors, numbers):
+    print(f"Phyrexian Praetor: {praetor}")
+    print(f"Power/Toughness: {number}")
+else:
+    print("End of list")
 
-# # FOR IN DICTIONARIES
+# FOR IN DICTIONARIES
 
-# red_praetor = dict(
-#     name="Urabrask",
-#     color="red",
-#     cost=[2, "R", "R"],
-#     power_toughness=4.4,
-#     legendary=True,
-# )
+red_praetor = dict(
+    name="Urabrask",
+    color="red",
+    cost=[2, "R", "R"],
+    power_toughness=4.4,
+    legendary=True,
+)
 
-# for key in red_praetor:
-#     print(key)
+for key in red_praetor:
+    print(key)
 
-# # ITEMS()
+# ITEMS()
 
-# for data in red_praetor.items():
-#     key = data[0]
-#     value = data[1]
-#     print(f"{key} = {value}")
+for data in red_praetor.items():
+    key = data[0]
+    value = data[1]
+    print(f"{key} = {value}")
 
 # CONTINUE & BREAK
 
@@ -77,3 +77,8 @@ green_praetor = "Vorinclex, Monstrous Raider"
 
 for letters in green_praetor:
     print(letters)
+
+# FOR IN "ONE LINE CODE"
+
+gains = [x + 2 for x in numbers]
+print(gains)
