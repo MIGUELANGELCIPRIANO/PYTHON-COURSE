affac4b869cba36dfc3687bb354157b9001408ad
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2024-05-16 15:26:25 -0300

    FEAT: Read txt files

---
+++ files/get_txt.py
@@ -0,0 +1,13 @@
+file = open("files\\text_file.txt", encoding="UTF-8")
+
+# READ COMPLETE FILE
+# print(file.read())
+
+# READ LINES FROM FILE
+# print(file.readlines())
+
+# READ FILE BY LINES
+# print(file.readline())
+
+# CLOSE FILE
+file.close()
