7db7e01db34faf7e18131ff926bbd7f5822d20c0
CIPRIANO <ciprianomiguelangel.dev@gmail.com>
2024-05-18 21:20:33 -0300

    FEAT: Exercise 3.1_'Working with files'

---
+++ exercise_3/exercise_3.1.py
@@ -0,0 +1,31 @@
+# WORKING WITH FILES
+
+names = [
+    "Leanne",
+    "Ervin",
+    "Clementine",
+    "Patricia",
+    "Chelsey",
+    "Dennis",
+    "Kurtis",
+    "Nicholas",
+    "Glenna",
+    "Clementina",
+]
+
+lastnames = [
+    "Graham",
+    "Howell",
+    "Bauch",
+    "Lebsack",
+    "Dietrich",
+    "Schulist",
+    "Weissnat",
+    "Runolfsdottir",
+    "Reichert",
+    "DuBuque",
+]
+
+with open("exercise_3\\users.txt", "w") as file:
+    file.writelines("Users:\n\n")
+    [file.writelines(f"Name: {n}\nLastname: {l}\n\n") for n, l in zip(names, lastnames)]
